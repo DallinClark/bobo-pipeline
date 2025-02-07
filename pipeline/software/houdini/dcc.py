3adc2aeddc19ca34b7f892e39ce21c4e4c76a408
Scott Milner <scottdmilner@gmail.com>
2025-02-06 17:03:37 -0700

    update RenderMan to be internal copy instead of default install

--- pipeline/software/houdini/dcc.py
+++ pipeline/software/houdini/dcc.py
@@ -97,7 +97,8 @@ class HoudiniDCC(DCC):
                 [
                     str(pipe_path),
                     # Add $RMANTREE/bin to PYTHONPATH for the Tractor PDG scheduler
-                    os.environ.get("RMANTREE", "") + "/bin",
+                    # os.environ.get("RMANTREE", "") + "/bin",
+                    str(get_production_path() / "opt/pixar/RenderManProServer-26.3"),
                 ]
             ),
             # RenderMan color config json file
