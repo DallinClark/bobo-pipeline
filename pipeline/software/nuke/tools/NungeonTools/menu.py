2c5ba2dd523b4da835dc3e5ce37bdcc89635c560
Scott Milner <scottdmilner@gmail.com>
2025-02-20 12:06:35 -0700

    NungeonTools updates from spencer

--- pipeline/software/nuke/tools/NungeonTools/menu.py
+++ pipeline/software/nuke/tools/NungeonTools/menu.py
@@ -6,6 +6,7 @@ nuke.pluginAddPath("./icons")
 nuke.pluginAddPath("./images")
 nuke.pluginAddPath("./nk_files")
 nuke.pluginAddPath("./toolsets")
+nuke.pluginAddPath("./scripts")
 
 # Nungeon buttons
 toolbar = nuke.menu("Nodes")
@@ -19,9 +20,24 @@ m.addCommand(
     f"nuke.nodePaste(\"{str(get_pipe_path() / 'software/nuke/tools/NungeonTools/toolsets/shotTemplate.nk')}\")",
     icon="nungeonIcon.png",
 )
+m.addCommand("FrameBurn", "nuke.createNode('FrameBurn')", icon="nungeonIcon.png")
 
 
-# asoect ratio
+########################### Shelf Tools#######################################
+LD_menu = nuke.menu("Nuke").addMenu("L&D Tools")
+
+# Open shot
+LD_menu.addCommand("Open Shot", "#do something here idk man", icon="openShot.jpg")
+
+# Render Layer Selector
+LD_menu.addCommand(
+    "Import Layers",
+    "import render_layer_selector; render_layer_selector.run()",
+    icon="rayden.jpg",
+)
+
+
+# aspect ratio
 nuke.addFormat("2048 870 Love_and_Dungeons_aspect_ratio")
 nuke.knobDefault("Root.format", "Love_and_Dungeons_aspect_ratio")
 
