7296b2b2494f2992a11154281f2ad67027851bf1
spencerBlanchard <spenseriv@gmail.com>
2025-02-20 13:04:30 -0700

    fixed render_layer_selector.py and added a shelf tool to run it

--- pipeline/software/nuke/tools/NungeonTools/menu.py
+++ pipeline/software/nuke/tools/NungeonTools/menu.py
@@ -1,11 +1,15 @@
 import nuke
 from shared.util import get_pipe_path
+import sys
+import os
+import webbrowser
 
 nuke.pluginAddPath("./gizmos")
 nuke.pluginAddPath("./icons")
 nuke.pluginAddPath("./images")
 nuke.pluginAddPath("./nk_files")
 nuke.pluginAddPath("./toolsets")
+nuke.pluginAddPath('./scripts')
 
 # Nungeon buttons
 toolbar = nuke.menu("Nodes")
@@ -21,8 +25,29 @@ m.addCommand(
 )
 
 
-# asoect ratio
+# aspect ratio
 nuke.addFormat("2048 870 Love_and_Dungeons_aspect_ratio")
 nuke.knobDefault("Root.format", "Love_and_Dungeons_aspect_ratio")
 
+
+#Shelf Tools 
+def import_render_layers():
+    import render_layer_selector
+    render_layer_selector.run()
+    print(1)
+
+
+def choose_shot():
+    nuke.message("Ah, this doesn't do anything quite yet sorry")
+
+menu = nuke.menu('Nuke')
+menu.addCommand('Choose Shot', 'choose_shot()')
+menu.addCommand('Import Render Layers', 'import_render_layers()')
+
+
+
+
+
+
 print("Nungeon loaded successfully")
+print("Isaac is a robot. If you train him the same way you train an AI model you will get good results.")
